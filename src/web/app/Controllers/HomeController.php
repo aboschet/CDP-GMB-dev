dddf91b5cfcbb77ab9a6a986c9d5f4a5c3f0ed0a
Antoine GAMELIN <contact@antoinegamelin.fr>
2016-11-03 09:41:19 +0100

    Correction bug structure

--- src/web/app/Controllers/HomeController.php
+++ src/web/app/Controllers/HomeController.php
@@ -4,11 +4,16 @@ namespace app\Controllers;
 use app\Core\AppController;
 use system\HTML\BootstrapForm;
 
-class DemoController extends AppController{
+class HomeController extends AppController{
 
     public function index(){
         $this->data['form'] =  new BootstrapForm($_POST);
         $this->render('demo', $this->data);
+    }
+
+    public function test($a, $b = 2) {
+        //echo $a;
+        echo $a+$b;
     }
 
 }
