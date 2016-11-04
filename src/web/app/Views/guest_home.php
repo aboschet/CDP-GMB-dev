94c3731796e14a021075d73fdc32df1ff8dd43ad
aboschet <antho.boschet@gmail.com>
2016-11-04 16:05:53 +0100

    Première version de la vue du formulaire d'inscription

--- src/web/app/Views/guest_home.php
+++ src/web/app/Views/guest_home.php
@@ -1,1 +1,84 @@
-Home page
+ <div class="col-md-6">
+
+  <h2  style="text-align:center">Inscription</h2>
+  <hr />
+   <form class="form-horizontal">
+
+    <div class="form-group">
+      <label class="control-label col-sm-2" >Pseudo :</label>
+      <div class="col-sm-10">
+        <input class="form-control" id="pseudo" placeholder="">
+      </div>
+    </div>
+
+     <div class="form-group">
+      <label class="control-label col-sm-2" >Nom :</label>
+      <div class="col-sm-10">
+        <input class="form-control" id="nom" placeholder="">
+      </div>
+    </div>
+
+     <div class="form-group">
+      <label class="control-label col-sm-2" >Prénom :</label>
+      <div class="col-sm-10">
+        <input class="form-control" id="prenom" placeholder="">
+      </div>
+    </div>
+
+     <div class="form-group">
+      <label class="control-label col-sm-2" >Email :</label>
+      <div class="col-sm-10">
+        <input class="form-control" id="email" placeholder="">
+      </div>
+    </div>
+
+    <div class="form-group">
+      <label class="control-label col-sm-2" >Mot de passe :</label>
+      <div class="col-sm-10">
+        <input class="form-control" id="motDePasse" placeholder="">
+      </div>
+    </div>
+
+
+    <div class="form-group">
+      <div class="col-sm-offset-6 col-sm-10">
+       <a href="#">
+          <span class="glyphicon glyphicon-ok" style="font-size: 2em"></span>
+        </a>
+      </div>
+    </div>
+
+  </form>
+</div>
+
+
+<div class="col-md-6">
+
+  <h2  style="text-align:center">Connexion</h2>
+<hr />
+   <form class="form-horizontal">
+
+    <div class="form-group">
+      <label class="control-label col-sm-2" >Pseudo :</label>
+      <div class="col-sm-10">
+        <input class="form-control" id="pseudo" placeholder="">
+      </div>
+    </div>
+
+    <div class="form-group">
+      <label class="control-label col-sm-2" >Mot de passe :</label>
+      <div class="col-sm-10">
+        <input class="form-control" id="motDePasse" placeholder="">
+      </div>
+    </div>
+
+
+    <div class="form-group">
+      <div class="col-sm-offset-6 col-sm-10">
+       <a href="#">
+          <span class="glyphicon glyphicon-ok" style="font-size: 2em"></span>
+        </a>
+      </div>
+    </div>
+  </form>
+</div>
