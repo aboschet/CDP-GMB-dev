355e502138523db13b2adfc68fc88a8807cb11bc
aboschet <antho.boschet@gmail.com>
2016-11-07 17:46:41 +0100

    Formulaire creation d'un projet ( manque le champs date et la liste des membres )

---
+++ src/web/app/Views/project/create.php
@@ -0,0 +1,57 @@
+
+  <h2  style="text-align:center">Créer un projet</h2>
+  <hr />
+   <form class="form-horizontal" action="" method="POST">
+
+    <div class="form-group">
+      <label class="control-label col-sm-2" style ="font-size : 85%" >Nom</label>
+      <div class="col-sm-10">
+        <input class="form-control" id="pseudo" name="pseudo" placeholder="">
+      </div>
+    </div>
+
+
+
+    <div class="col-md-6">
+      
+
+      <div class="form-group">
+        <label class="control-label col-sm-2" style ="font-size : 85%">Description</label>
+         <div class="col-sm-10">
+        <textarea class="form-control" id="desciption" rows="10"></textarea>
+      </div>
+      </div>     
+  </div>
+
+
+
+
+   <div class="col-md-6">
+      <div class="form-group">
+        <label class="control-label col-sm-2" style ="font-size : 85%" >URL Github Dev</label>
+        <div class="col-sm-10">
+          <input class="form-control" id="motDePasse" name="motDePasse" placeholder="">
+        </div>
+      </div>
+
+      <div class="form-group">
+        <label class="control-label col-sm-2" style ="font-size : 85%" >URL Github Demo</label>
+        <div class="col-sm-10">
+          <input class="form-control" id="motDePasse" name="motDePasse"placeholder="">
+        </div>
+      </div>
+
+    </div>
+
+     <div class="form-group">
+        <div class="col-sm-offset-6 col-sm-10">
+         <button class="btn btn-primary">
+            <span class="glyphicon glyphicon-ok" style="font-size: 2em"></span>
+          </button>
+        </div>
+      </div>
+
+
+
+    </form>
+  </div>
