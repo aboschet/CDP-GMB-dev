85ea6a80df3d12b6585862637dc905a9578372a5
nab-bila <nabila.mokadmi@gmail.com>
2016-11-17 13:28:03 +0100

    Create SprintsEntity.php

---
+++ src/web/app/Entities/SprintsEntity/SprintsEntity.php
@@ -0,0 +1,16 @@
+<?php
+namespace app\Entities;
+
+use system\Entity\Entity;
+
+class SprintsEntity extends Entity{
+
+  public function getDateDebutSprint() {
+    return date('d/m/Y' , strtotime($this->dateDebut));
+  }
+  
+  public function getDateFinSprint() {
+    return date('d/m/Y' , strtotime($this->dateFin));
+  }
+  
+}
