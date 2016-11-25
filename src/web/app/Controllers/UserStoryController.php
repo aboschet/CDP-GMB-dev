bf84154a46f0b64e36eceb936d63ef19d700e32d
aboschet <antho.boschet@gmail.com>
2016-11-25 12:01:08 +0100

    Traçabilité non terminée

--- src/web/app/Controllers/UserStoryController.php
+++ src/web/app/Controllers/UserStoryController.php
@@ -68,7 +68,40 @@ class UserStoryController extends AppController{
     }
     
     public function traceability() {
+      $id = $_SESSION['project_id'];
+       if(isset($_SESSION['message'])) {
+        $this->data['message'] = $_SESSION['message'];
+        unset($_SESSION['message']);
+      }
+      
+      if(isset($_SESSION['error'])) {
+        $this->data['error'] = $_SESSION['error'];
+        unset($_SESSION['error']);
+      }
+      $this->data['userstories'] = $this->UserStories->getUS($id);
+      $this->data['isOwner'] = $this->Projects->isOwner($id);
       $this->render('us/traceability', $this->data);
+    }
+
+    public function insertNumCommit ($idUs) {
+     // $this->UserStories->insert(array('idProjet' => 2, 'nom' => "bonjour" , 'chiffrage' => 1, 'priorite' => 3));
+      $this->UserStories->update(array('id'=>1),array('numCommit' => "bonourdjkhf"));        
+     /* $id = $_SESSION['project_id'];
+
+      $rules = FormValidation::is_valid($_POST, array(
+        'numCommit' => 'required'
+      ));
+        
+
+      if($rules === true) { 
+        $_POST['idProjet'] = $id;
+        //$this->UserStories->update(array('id'=>1), array('numCommit' =>3));
+
+      }
+       else {
+        $_SESSION['error'] = $rules;
+      }
+     //$this->redirect(BASE_URL.'UserStory/traceability');*/
     }
         
     
--- src/web/app/Views/us/traceability.php
+++ src/web/app/Views/us/traceability.php
@@ -1,17 +1,46 @@
-  <?php if(isset($error)) : ?>
-  <div class="col-md-12 alert alert-danger">
-    <?php 
-    foreach($error as $value) {
-        echo "<p>".$value."</p>";
-     }?>
-  </div>
- <?php endif; ?>
- <?php if(isset($message)) : ?>
-  <div class="col-md-12 alert alert-success">
-    <?= $message; ?>
-  </div>
- <?php endif; ?>
- 
- <div class="col-md-12 alert alert-success">
-   Not yet implemented
-  </div>
+<div class="container">
+  <h2>Traçabilité</h2>
+  <table class="table table-striped">
+    <thead>
+      <tr>
+        <th>User Story</th>
+        <th>Numéro du commit</th>
+      </tr>
+    </thead>
+    <tbody>
+
+      <?php 
+        foreach($userstories as $userstory) { ?>
+          <tr>
+            <td><?= $userstory->nom; ?></td>
+
+            <?php if ($userstory->numCommit == NULL) { ?>
+              <td>  <form class="form-inline" action="<?= BASE_URL.'UserStory/insertNumCommit/'.$userstory->id; ?>" method="POST">
+                    <input class="form-control" id="numCommit" placeholder="Numéro du commit">
+                    <button type="submit" class="btn btn-default">Valider</button>
+                   </form>
+            </td>
+            <?php } else { ?>
+                      <td><?= $userstory->numCommit ?></td>
+            <?php } ?>
+          </tr>
+        <?php } ?>
+
+      <tr>
+        <td>John</td>
+        <td>Doe</td>
+      </tr>
+
+      <tr>
+        <td>Mary</td>
+        <td>Moe</td>
+      </tr>
+
+      <tr>
+        <td>July</td>
+        <td>Dooley</td>
+      </tr>
+
+    </tbody>
+  </table>
+</div>
