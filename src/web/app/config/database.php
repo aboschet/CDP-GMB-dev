d997a25e5efd45cfb7016afca3e37d7d5af7ca68
aboschet <antho.boschet@gmail.com>
2016-12-01 20:27:42 +0100

    Ajout du test permettant l'affichage des projets publics

--- src/web/app/config/database.php
+++ src/web/app/config/database.php
@@ -1,7 +1,14 @@
 <?php
-return array(
+/*return array(
     "db_user" => "cdp",
     "db_pass" => "cdp",
     "db_host" => "cdp.antoinegamelin.fr",
+    "db_name" => "cdp"
+);*/
+
+return array(
+    "db_user" => "root",
+    "db_pass" => "",
+    "db_host" => "localhost",
     "db_name" => "cdp"
 );
